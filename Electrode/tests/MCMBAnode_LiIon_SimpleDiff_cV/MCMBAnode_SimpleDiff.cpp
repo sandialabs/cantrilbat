1b8f4312f3e642bf015ae33bb0112c0a1338dd5e
hkmoffa <hkmoffa@sandia.gov>
2014-01-24 14:40:43 -0700

    Implemented the formulationType_ variable

--- Electrode/tests/MCMBAnode_LiIon_SimpleDiff_cV/MCMBAnode_SimpleDiff.cpp
+++ Electrode/tests/MCMBAnode_LiIon_SimpleDiff_cV/MCMBAnode_SimpleDiff.cpp
@@ -173,6 +173,7 @@ int main(int argc, char **argv)
 
  //   electrodeA->enableExtraPrinting_ = true;
     electrodeA->detailedResidPrintFlag_ = 4;
+    electrodeA->formulationType_ = 2;
   
     for (int itimes = 0; itimes < nT; itimes++) {
       Tinitial = Tfinal;
