4f9e9b306a36eaf7e4ee84d32d42476ecb38bdad
Harry Moffat <hkmoffa@sandia.gov>
2015-04-15 18:15:17 -0600

    Added the needed file to run the test harness

---
+++ Electrode/tests/LiSi_5_Anode_Integrate/problem_setup.sh
@@ -0,0 +1,30 @@
+#!/bin
+#
+#
+#  Name of the program to run
+#
+PROGRAM=anode_5_intSurf 
+#
+#  Program input file
+#
+PROGRAM_INPUT=
+#
+#  Any other program options
+#
+PROGRAM_OPTS=''
+#
+#
+BLESSED_DATA_FILES=" good_out.txt "
+DATA_FILES="         out.txt      "
+DIFF_NAMES="         diff_out.txt "
+DIFF_REQ="           True  "  
+#
+BLESSED_CSV_FILES=
+CSV_FILES=
+DIFF_CSV_NAMES=
+DIFF_CSV_REQ=
+
+#
+#  Extra files to be removed before the test starts
+#
+EXTRA_WHACKED_FILES=''
