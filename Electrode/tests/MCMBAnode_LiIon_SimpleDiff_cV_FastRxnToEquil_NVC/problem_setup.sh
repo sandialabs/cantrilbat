901070e2de7ce4da23755efca42a8a8bf39905b2
Harry Moffat <hkmoffa@sandia.gov>
2015-09-10 09:40:11 -0600

    Added test to test harness

---
+++ Electrode/tests/MCMBAnode_LiIon_SimpleDiff_cV_FastRxnToEquil_NVC/problem_setup.sh
@@ -0,0 +1,37 @@
+#!/bin
+#
+#     Problem setup for runtest_Electrode
+#
+WD=`pwd`
+TEST_NAME=`basename $WD`
+#
+#  Name of the program to run
+#
+PROGRAM=MCMBAnode_SimpleDiff_Frxn_NVC
+#
+#  Any other program options
+#
+PROGRAM_OPTS=''
+#
+#  
+SOLUTION_BLESSED_XML="   soln_0_0_blessed.xml "
+SOLUTION_XML="           soln_0_0.xml         "
+SOLUTION_XML_DIFFNAME="  diff_xml.txt         "
+#
+#   Text files to compare against
+#
+BLESSED_DATA_FILES=" good_out.txt  "
+DATA_FILES="         out.txt       "
+DIFF_NAMES="         diff_out.txt  "
+DIFF_REQ="           True          "
+#
+#  CSV file to compare against
+#
+BLESSED_CSV_FILES="  MCMB_globalResults_blessed_0_0.csv  MCMB_intResults_blessed_0_0.csv "
+CSV_FILES="          MCMB_globalResults_0_0.csv  MCMB_intResults_0_0.csv "
+DIFF_CSV_NAMES="     diff_gcsv.txt                 diff_icsv.txt     "
+DIFF_CSV_REQ="       True                     True "
+#
+#  Extra files to be removed before the test starts
+#
+EXTRA_WHACKED_FILES=''
