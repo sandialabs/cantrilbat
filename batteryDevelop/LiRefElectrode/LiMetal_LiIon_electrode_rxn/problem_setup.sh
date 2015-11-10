4d2717355dbd43268114fd1ebcd745ca5d322f39
Harry Moffat <hkmoffa@sandia.gov>
2015-11-09 19:55:37 -0700

    Added a dir for the old way of doing things.

---
+++ batteryDevelop/LiRefElectrode/LiMetal_LiIon_electrode_rxn/problem_setup.sh
@@ -0,0 +1,32 @@
+#!/bin
+#
+#
+#  Name of the program to run
+#
+PROGRAM=cttables
+#
+# Extra command line arguments
+#
+PROGRAM_EXTRA_OPTS=
+#
+# Standard input for program (leave commented if none)
+#
+# PROGRAM_STD_INPUT=
+#
+#
+#
+#
+BLESSED_DATA_FILES=" good_cout.txt  "
+DATA_FILES="         out.txt         "
+DIFF_NAMES="         diff_cout.txt     "
+DIFF_REQ="           True           " 
+#
+BLESSED_CSV_FILES="   "
+CSV_FILES="                  "
+DIFF_CSV_NAMES="            "
+DIFF_CSV_REQ="                  " 
+#
+#
+#  Extra files to be removed before the test starts
+#
+EXTRA_WHACKED_FILES=''
