8ebea29bebf79ae6ffe39a6373eace04198f9ae8
Harry Moffat <hkmoffa@sandia.gov>
2016-01-11 16:42:38 -0700

    Added a couple of lines of proposed global fully coupled electrode interface

---
+++ Electrode/src/Electrode_GFCE.h
@@ -0,0 +1,30 @@
+//! This class is a derived class used to model phase - change electrodes
+/*!
+ * Complete problem statement
+ *
+
+*/
+class Electrode_GFCE : public Cantera::ResidJacEval
+{
+public:
+
+
+
+    //! Pointer to the electrode object
+    Electrode_Integrator*  electrode_;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+};
+
