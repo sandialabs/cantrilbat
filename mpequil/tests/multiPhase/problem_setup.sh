581a79b5b235513c4d2f69a159b9575cb757ce3b
Harry Moffat <hkmoffa@sandia.gov>
2016-02-02 11:12:45 -0700

    Modified a test to use the new test harness

---
+++ mpequil/tests/multiPhase/problem_setup.sh
@@ -0,0 +1,33 @@
+#!/bin
+#
+#     Problem setup for runtest_Electrode
+#
+TEST_NAME=mpequil_multiPhase
+#
+#  Name of the program to run
+#
+PROGRAM=mpequil
+#
+#  Any other program options
+#
+PROGRAM_OPTS=' mpequil.inp'
+#
+#  
+#
+#   Text files to compare against
+#
+BLESSED_DATA_FILES=" good_out.txt  "
+DATA_FILES="         out.txt       "
+DIFF_NAMES="         diff_out.txt  "
+DIFF_REQ="           True          "
+#
+#  CSV file to compare against
+#
+BLESSED_CSV_FILES="  vcs_equilibrate_blessed.csv "
+CSV_FILES="          vcs_equilibrate_res.csv        "
+DIFF_CSV_NAMES="     diff_csv.txt    "
+DIFF_CSV_REQ="       True            "
+#
+#  Extra files to be removed before the test starts
+#
+EXTRA_WHACKED_FILES=''
