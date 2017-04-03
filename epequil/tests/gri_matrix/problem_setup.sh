0d1ce8281f20a5b5fa36c2865787be1c43204776
Harry Moffat <hkmoffa@sandia.gov>
2017-04-03 16:00:26 -0600

    Reference pressure Clarification -> changing reference pressure to value actually used in the code after current hard-code intervention.

---
+++ epequil/tests/gri_matrix/problem_setup.sh
@@ -0,0 +1,33 @@
+#!/bin
+#
+#     Problem setup for runtest_Electrode
+#
+TEST_NAME=gri_matrix
+#
+#  Name of the program to run
+#
+PROGRAM=gri_matrix
+#
+#  Any other program options
+#
+PROGRAM_OPTS=' '
+#
+#  
+#
+#   Text files to compare against
+#
+BLESSED_DATA_FILES=" output_blessed.txt  "
+DATA_FILES="         output.txt       "
+DIFF_NAMES="         diff_out.txt  "
+DIFF_REQ="           True          "
+#
+#  CSV file to compare against
+#
+BLESSED_CSV_FILES="   "
+CSV_FILES="               "
+DIFF_CSV_NAMES="      "
+DIFF_CSV_REQ="               "
+#
+#  Extra files to be removed before the test starts
+#
+EXTRA_WHACKED_FILES=''
