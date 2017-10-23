9540af51b48ef27f84692bd2cc418c3c62fd81c5
Harry Moffat <hkmoffa@sandia.gov>
2017-10-23 16:05:25 -0600

    Added a test problem for NonlinearSoln_JAC

---
+++ Electrode/tests_develop/MCMBAnode_LiIon_SimpleDiff_cV/README.txt
@@ -0,0 +1,8 @@
+
+10/23/2017
+This is a very simple test that prints out a lot of output.
+
+It's meant to test the Transfer of NonlinearSolver to a JacobianManager formulation
+
+It actually prints out the Numerical jacobian to stdout. It's expected that the nonlinear
+iteration behavior and the Numerical jacobian will stay constant as a function of time.
---
+++ Electrode/tests_develop/MCMBAnode_LiIon_SimpleDiff_cV/problem_setup.sh
@@ -0,0 +1,36 @@
+#!/bin
+#
+#     Problem setup for runtest_Electrode
+#
+TEST_NAME=MCMBAnode_LiIon_SimpleDiff_cV
+#
+#  Name of the program to run
+#
+PROGRAM=MCMBAnode_SimpleDiff
+#
+#  Any other program options
+#
+PROGRAM_OPTS=''
+#
+#  
+SOLUTION_BLESSED_XML="   soln_0_0_blessed.xml "
+SOLUTION_XML="           soln_0_0.xml         "
+SOLUTION_XML_DIFFNAME="  diff_xml.txt         "
+#
+#   Text files to compare against
+#
+BLESSED_DATA_FILES=" good_out.txt  "
+DATA_FILES="         out.txt       "
+DIFF_NAMES="         diff_out.txt  "
+DIFF_REQ="           True          "
+#
+#  CSV file to compare against
+#
+BLESSED_CSV_FILES=" MCMB_intResults_blessed_0_0.csv  MCMB_globalResults_blessed_0_0.csv   "
+CSV_FILES="         MCMB_intResults_0_0.csv          MCMB_globalResults_0_0.csv "
+DIFF_CSV_NAMES="     diff_csv.txt                    diff_gcsv.txt "
+DIFF_CSV_REQ="       True                            True"
+#
+#  Extra files to be removed before the test starts
+#
+EXTRA_WHACKED_FILES=''
